23d40786af8377ab8e1893da1c63a24e92e2e71e
Jofay-zs <jofayzs19@gmail.com>
2022-06-12 23:08:29 -0500

    [ADD] version #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./containers/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NewPassword from "./pages/NewPassword";
+import RecoveryPassword from "./pages/RecoveryPassword";
+import NotFound from "./pages/NotFound";
+import MyAccount from "./pages/MyAccount";
+import Register from "./pages/Register";
+import SendEmail from "./pages/SendEmail";
+import Checkout from "./pages/Checkout";
+import Orders from "./pages/Orders";
+import AppContext from "./context/AppContext";
+import useInitialState from "./hooks/useInitialState";
+import "./styles/global.scss";
+
 function App() {
+  const initialState = useInitialState();
+
   return (
-    <div className="App">
-      I will create an E-commerce with React and TypeScript
-    </div>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="new-password" element={<NewPassword />} />
+            <Route path="recovery-password" element={<RecoveryPassword />} />
+            <Route path="send-email" element={<SendEmail />} />
+            <Route path="account" element={<MyAccount />} />
+            <Route path="register" element={<Register />} />
+            <Route path="checkout" element={<Checkout />} />
+            <Route path="orders" element={<Orders />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
---
+++ src/components/ResponsiveNavbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/components/responsive-navbar.scss";
+
+const ResponsiveNavbar = () => {
+  return (
+    <nav className="responsive-navbar">
+      <div className="responsive-navbar-categories">
+        <h4>CATEGORIES</h4>
+        <ul>
+          <li>
+            <Link to="/">All</Link>
+          </li>
+          <li>
+            <Link to="/">Clothes</Link>
+          </li>
+          <li>
+            <Link to="/">Electronics</Link>
+          </li>
+          <li>
+            <Link to="/">Furnitures</Link>
+          </li>
+          <li>
+            <Link to="/">Toys</Link>
+          </li>
+          <li>
+            <Link to="/">Others</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="responsive-navbar-account">
+        <ul>
+          <li>
+            <a href="/">My orders</a>
+          </li>
+          <li>
+            <a href="/">My account</a>
+          </li>
+          <li>
+            <a href="/">Sign out</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default ResponsiveNavbar;
